174f90acb8430bfc7bfdd06a9b0009eb64b08f6d
exthazor <rhitamdutta1996@gmail.com>
2026-01-10 02:56:21 +0530

    Add other imports

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  pool,
+};
