963084310c743a4f1c769b325cc34dd10f8d4d1b
exthazor <rhitamdutta1996@gmail.com>
2026-01-11 17:36:33 +0530

    Increase nginx size

--- src/config/s3.mjs
+++ src/config/s3.mjs
@@ -1,10 +1,19 @@
 import AWS from "aws-sdk";
 import config from "./env.mjs";
 
-const s3 = new AWS.S3({
-  accessKeyId: config.aws.accessKeyId,
-  secretAccessKey: config.aws.secretAccessKey,
+// When running on Elastic Beanstalk, credentials are automatically
+// provided via the IAM instance role. Only use explicit credentials
+// for local development.
+const s3Config = {
   region: config.aws.region,
-});
+};
+
+// Use explicit credentials only if provided (local development)
+if (config.aws.accessKeyId && config.aws.secretAccessKey) {
+  s3Config.accessKeyId = config.aws.accessKeyId;
+  s3Config.secretAccessKey = config.aws.secretAccessKey;
+}
+
+const s3 = new AWS.S3(s3Config);
 
 export default s3;
